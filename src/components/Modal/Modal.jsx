1a1ccf974457905f73ed0a8ae54cb31bd43f17e9
Serg1853 <129977140+Serg1853@users.noreply.github.com>
2023-11-03 15:10:46 +0200

    useEffect function

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,19 +3,18 @@ import { useEffect } from 'react';
 
 export const Modal = ({ selectedPhoto: { largeImageURL, tags }, onClose }) => {
   useEffect(() => {
+    function onEscapeCloseModal(event) {
+      if (event.code === 'Escape') {
+        onClose();
+      }
+    }
     window.addEventListener('keydown', onEscapeCloseModal);
     document.body.style.overflow = 'hidden';
     return () => {
       window.removeEventListener('keydown', onEscapeCloseModal);
       document.body.style.overflow = 'auto';
     };
-  }, [onEscapeCloseModal]);
-
-  function onEscapeCloseModal(event) {
-    if (event.code === 'Escape') {
-      onClose();
-    }
-  }
+  }, [onClose]);
 
   const onClickOverlay = event => {
     if (event.target === event.currentTarget) {
